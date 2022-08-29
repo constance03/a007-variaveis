e7c064c315b9ea8ac6f435f63605677503268fd7
constance03 <maria-constance@hotmail.com>
2022-08-29 14:27:56 -0300

    exercicio 3 feito

---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+//Duas variaveis a e b
+let a = 10
+let b = 25
+
+//trocar os valores de a e b
+let novoA = b
+let novoB = a
+a = novoA
+b = novoB
+
+
+//imprimir os novos valores de a e b
+console.log("o novo valor de a é:", a)
+console.log("o novo valor de b é:", b)
